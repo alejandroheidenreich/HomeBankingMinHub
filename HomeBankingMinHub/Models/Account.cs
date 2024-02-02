2cff14ee2bdae3608c98ab058525cd8517e732b5
Ale Heidenreich <heidenreich.alejandro@gmail.com>
2024-02-02 11:03:05 -0300

    Class Account added

---
+++ HomeBankingMinHub/Models/Account.cs
@@ -0,0 +1,11 @@
+﻿namespace HomeBankingMinHub.Models
+{
+    public class Account
+    {
+        public long Id { get; set; }
+        public string Number { get; set; }
+        public DateTime CreationDate { get; set; }
+        public double Balance { get; set; }
+        public long ClientId { get; set; }
+    }
+}
