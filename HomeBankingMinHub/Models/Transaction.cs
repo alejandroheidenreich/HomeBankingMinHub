bc213630f752ae7db7c21d2842c97ae102cd7e37
Ale Heidenreich <heidenreich.alejandro@gmail.com>
2024-02-05 11:29:10 -0300

    Class Transaction added

---
+++ HomeBankingMinHub/Models/Transaction.cs
@@ -0,0 +1,12 @@
+﻿namespace HomeBankingMinHub.Models
+{
+    public class Transaction
+    {
+        public long Id { get; set; }
+        public string Type { get; set; }
+        public double Amount { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public long AccountId { get; set; }
+    }
+}
